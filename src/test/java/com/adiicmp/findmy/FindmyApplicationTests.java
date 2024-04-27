6ccdfc01b896f2e70bab2d4d1ba599645301b6db
Adrian Câmpean <aicampean@gmail.com>
2024-04-27 17:13:32 +0300

    Moving the project from bimj0rk/Web-Development to its own repo

---
+++ src/test/java/com/adiicmp/findmy/FindmyApplicationTests.java
@@ -0,0 +1,11 @@
+package com.adiicmp.findmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmyApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
